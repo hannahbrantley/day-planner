2a978eb730833bd99189159c9d64ef6621fe797c
Hannah Brantley <hannahbrantley@Hannahs-MacBook-Pro.local>
2020-05-31 16:03:00 -0600

    add logic for active goals

--- models/goal.js
+++ models/goal.js
@@ -6,11 +6,16 @@ const goalSchema = new Schema({
   details: String,
   startDate: { type: Date, required: true },
   dueDate: { type: Date, required: true }, 
-  active: { type: Boolean, default: true },  
+  // active: { type: Boolean, default: true },  
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User'
   }
 });
+
+goalSchema.virtual('active').get(function() {
+  var today = new Date();
+  if (this.startDate <= today && this.dueDate >= today) return true;
+})
 
 module.exports = mongoose.model('Goal', goalSchema);
