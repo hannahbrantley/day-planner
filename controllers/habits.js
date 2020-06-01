abd516bdbd93ac7163231daebee39b3da741f175
Hannah Brantley <hannahbrantley@Hannahs-MacBook-Pro.local>
2020-05-31 21:50:34 -0600

    Create habit - buggy

--- controllers/habits.js
+++ controllers/habits.js
@@ -1,7 +1,10 @@
 const Habit = require('../models/habit');
+const Goal = require('../models/goal');
 
 module.exports = {
-  index
+  index, 
+  new: newHabit,
+  create
 }
 
 function index(req, res) {
@@ -13,3 +16,24 @@ function index(req, res) {
     });
   });
 }
+
+function newHabit(req, res) {
+  Goal.find({}, function (err, goals) {
+    res.render('habits/new', {
+      goals,
+      title: 'Habits', 
+      user: req.user,
+    });
+  })
+}
+
+function create(req, res) {
+  Goal.findById(req.body.goalId, function (err, goal) {
+    req.body.goal = goal;
+    console.log(req.body);
+    Habit.create(req.body, function(err, newHabit) {
+      newHabit.goal.push(goal);
+    })
+  })
+}
+
--- routes/habits.js
+++ routes/habits.js
@@ -2,6 +2,8 @@ var router = require('express').Router();
 var habitsCtrl = require('../controllers/habits');
 
 router.get('/', isLoggedIn, habitsCtrl.index);
+router.get('/new', isLoggedIn, habitsCtrl.new);
+router.post('/', isLoggedIn, habitsCtrl.create);
 
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) return next();
